9c86456ae20912b8a95b661e4178426509701445
jassielCota <159939201+jassielCota@users.noreply.github.com>
2024-02-13 10:13:09 -0700

    Add files via upload

---
+++ Ejemplo/src/Main.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame ventana=new JFrame("example");
+		ventana.setVisible(true);
+		ventana.setSize(100,100);
+		ventana.setLocation(100,100);
+		
+		
+		
+
+	}
+
+}
