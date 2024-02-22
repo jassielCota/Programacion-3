3cfff29eaf60d82e19055cd94804500e4ed774be
jassielCota <159939201+jassielCota@users.noreply.github.com>
2024-02-22 08:39:27 -0700

    unidad 2: ejercicio 19

--- Ejemplo/src/Ventana.java
+++ Ejemplo/src/Ventana.java
@@ -3,9 +3,14 @@ import java.awt.Dimension;
 import java.awt.Font;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
@@ -32,132 +37,9 @@ public class Ventana extends JFrame {
 	}
 	public void iniciarComponentes() {
 		
-		this.login();
-		//panel derecho
-		JPanel registro=new JPanel();
-		registro.setSize(this.getWidth()/2,this.getHeight());
-		registro.setLocation(500,0);
-		registro.setBackground(Color.pink);
-		registro.setLayout(null);
-		this.add(registro);
-		
-		//crear titulo "registro"
-		JLabel regis=new JLabel("Registro",0);
-	    regis.setSize(200,60);
-	    regis.setFont(new Font("Arial",Font.BOLD,20));
-	    regis.setLocation(145,30);
-		regis.setOpaque(true);
-		regis.setBackground(Color.blue);
-		regis.setLayout(null);
-		registro.add(regis);
-		
-		//recuadro "nombre de usuario"
-		JLabel nomUsuario=new JLabel("Nombre de usuario");
-		nomUsuario.setFont(new Font("Arial",Font.BOLD,12));
-		nomUsuario.setBounds(145,120,200,35);
-		nomUsuario.setLayout(null);
-		nomUsuario.setOpaque(true);
-		nomUsuario.setHorizontalAlignment(0);
-		registro.add(nomUsuario);
-		
-		JTextField userBox=new JTextField();
-		userBox.setBounds(100,160,290,35);
-		userBox.setHorizontalAlignment(0);
-		registro.add(userBox);
-		
-		//recuadro "bio"
-		JLabel bio=new JLabel("Bio");
-		bio.setFont(new Font("Arial",Font.BOLD,12));
-		bio.setBounds(145,200,200,35);
-		bio.setLayout(null);
-		bio.setOpaque(true);
-		bio.setHorizontalAlignment(0);
-		registro.add(bio);
-		
-		JTextArea userBoxBio=new JTextArea();
-		userBoxBio.setBounds(100,245,290,60);
-		userBoxBio.setFont(new Font("Arial",Font.BOLD,14));
-		registro.add(userBoxBio);
-		
-		//recuadro tachable de "Dulces"		
-		JCheckBox item_1=new JCheckBox("Dulces");
-		item_1.setFont(new Font("Arial",Font.BOLD,12));
-		item_1.setBounds(20,350,100,35);
-		item_1.setOpaque(false);
-		registro.add(item_1);
-		
-		//recuadro tachable de "Dulces"		
-		JCheckBox item_2=new JCheckBox("salado");
-		item_2.setFont(new Font("Arial",Font.BOLD,12));
-		item_2.setBounds(100,350,100,35);
-		item_2.setOpaque(false);
-		registro.add(item_2);
-		
-		//recuadro tachable de "saludable"		
-		JCheckBox item_3=new JCheckBox("saludable");
-		item_3.setFont(new Font("Arial",Font.BOLD,12));
-		item_3.setBounds(200,350,100,35);
-		item_3.setOpaque(false);
-		registro.add(item_3);
-		
-		//leyenda "preferencias"
-		JLabel prefer=new JLabel("Preferencias");
-		prefer.setFont(new Font("Arial",Font.BOLD,16));
-		prefer.setBounds(145,320,200,35);
-		prefer.setLayout(null);
-		prefer.setOpaque(false);
-		prefer.setHorizontalAlignment(0);
-		registro.add(prefer);
-		
-		//recuadro "Terminos"
-		JLabel terminos=new JLabel("Terminos");
-		terminos.setFont(new Font("Arial",Font.BOLD,12));
-		terminos.setBounds(145,390,200,35);
-		terminos.setLayout(null);
-		terminos.setOpaque(true);
-		terminos.setHorizontalAlignment(0);
-		registro.add(terminos);
-		
-		JRadioButton acepto_radio=new JRadioButton("Acepto los terminos");
-		acepto_radio.setBounds(20,430,200,35);
-		acepto_radio.setFont(new Font("Arial",Font.BOLD,12));
-		acepto_radio.setOpaque(false);
-		registro.add(acepto_radio);
-		
-		JRadioButton noAcepto_radio=new JRadioButton("No Acepto los terminos");
-		noAcepto_radio.setBounds(200,430,200,35);
-		noAcepto_radio.setFont(new Font("Arial",Font.BOLD,12));
-		noAcepto_radio.setOpaque(false);
-		registro.add(noAcepto_radio);
-		
-		ButtonGroup grupo=new ButtonGroup();
-		grupo.add(acepto_radio);
-		grupo.add(noAcepto_radio);
-		
-		String colonias[]= {"centro","pedregal","conchalito","camino real"};
-		
-		JComboBox location =new JComboBox(colonias);
-		location.setBounds(200,500,200,35);
-		location.setFont(new Font("Arial",Font.BOLD,12));
-		registro.add(location);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
-		
-		
-		
-		
+		//this.login();
+		//this.registro();
+		this.admin();
 	}
 	public void login() {
 		JPanel login=new JPanel();
@@ -217,5 +99,200 @@ public class Ventana extends JFrame {
 		
 	}
 	
+	public void registro () {
+		//panel derecho
+				JPanel registro=new JPanel();
+				registro.setSize(this.getWidth()/2,this.getHeight());
+				registro.setLocation(500,0);
+				registro.setBackground(Color.pink);
+				registro.setLayout(null);
+				this.add(registro);
+				
+				//crear titulo "registro"
+				JLabel regis=new JLabel("Registro",0);
+			    regis.setSize(200,60);
+			    regis.setFont(new Font("Arial",Font.BOLD,20));
+			    regis.setLocation(145,30);
+				regis.setOpaque(true);
+				regis.setBackground(Color.blue);
+				regis.setLayout(null);
+				registro.add(regis);
+				
+				//recuadro "nombre de usuario"
+				JLabel nomUsuario=new JLabel("Nombre de usuario");
+				nomUsuario.setFont(new Font("Arial",Font.BOLD,12));
+				nomUsuario.setBounds(145,120,200,35);
+				nomUsuario.setLayout(null);
+				nomUsuario.setOpaque(true);
+				nomUsuario.setHorizontalAlignment(0);
+				registro.add(nomUsuario);
+				
+				JTextField userBox=new JTextField();
+				userBox.setBounds(100,160,290,35);
+				userBox.setHorizontalAlignment(0);
+				registro.add(userBox);
+				
+				//recuadro "bio"
+				JLabel bio=new JLabel("Bio");
+				bio.setFont(new Font("Arial",Font.BOLD,12));
+				bio.setBounds(145,200,200,35);
+				bio.setLayout(null);
+				bio.setOpaque(true);
+				bio.setHorizontalAlignment(0);
+				registro.add(bio);
+				
+				JTextArea userBoxBio=new JTextArea();
+				userBoxBio.setBounds(100,245,290,60);
+				userBoxBio.setFont(new Font("Arial",Font.BOLD,14));
+				registro.add(userBoxBio);
+				
+				//recuadro tachable de "Dulces"		
+				JCheckBox item_1=new JCheckBox("Dulces");
+				item_1.setFont(new Font("Arial",Font.BOLD,12));
+				item_1.setBounds(20,350,100,35);
+				item_1.setOpaque(false);
+				registro.add(item_1);
+				
+				//recuadro tachable de "Dulces"		
+				JCheckBox item_2=new JCheckBox("salado");
+				item_2.setFont(new Font("Arial",Font.BOLD,12));
+				item_2.setBounds(100,350,100,35);
+				item_2.setOpaque(false);
+				registro.add(item_2);
+				
+				//recuadro tachable de "saludable"		
+				JCheckBox item_3=new JCheckBox("saludable");
+				item_3.setFont(new Font("Arial",Font.BOLD,12));
+				item_3.setBounds(200,350,100,35);
+				item_3.setOpaque(false);
+				registro.add(item_3);
+				
+				//leyenda "preferencias"
+				JLabel prefer=new JLabel("Preferencias");
+				prefer.setFont(new Font("Arial",Font.BOLD,16));
+				prefer.setBounds(145,320,200,35);
+				prefer.setLayout(null);
+				prefer.setOpaque(false);
+				prefer.setHorizontalAlignment(0);
+				registro.add(prefer);
+				
+				//recuadro "Terminos"
+				JLabel terminos=new JLabel("Terminos");
+				terminos.setFont(new Font("Arial",Font.BOLD,12));
+				terminos.setBounds(145,390,200,35);
+				terminos.setLayout(null);
+				terminos.setOpaque(true);
+				terminos.setHorizontalAlignment(0);
+				registro.add(terminos);
+				
+				JRadioButton acepto_radio=new JRadioButton("Acepto los terminos");
+				acepto_radio.setBounds(20,430,200,35);
+				acepto_radio.setFont(new Font("Arial",Font.BOLD,12));
+				acepto_radio.setOpaque(false);
+				registro.add(acepto_radio);
+				
+				JRadioButton noAcepto_radio=new JRadioButton("No Acepto los terminos");
+				noAcepto_radio.setBounds(200,430,200,35);
+				noAcepto_radio.setFont(new Font("Arial",Font.BOLD,12));
+				noAcepto_radio.setOpaque(false);
+				registro.add(noAcepto_radio);
+				
+				ButtonGroup grupo=new ButtonGroup();
+				grupo.add(acepto_radio);
+				grupo.add(noAcepto_radio);
+				
+				String colonias[]= {"Ninguna","Centro","Pedregal","Conchalito","Camino real"};
+				
+				JComboBox location =new JComboBox(colonias);
+				location.setBounds(200,500,200,35);
+				location.setFont(new Font("Arial",Font.BOLD,12));
+				registro.add(location);
+				
+		
+	}
+	
+	public void admin() {
+		JPanel admin_panel =new JPanel();
+		admin_panel.setSize(this.getWidth(),this.getHeight());
+		admin_panel.setLocation(0,0);
+		admin_panel.setBackground(Color.pink);
+		admin_panel.setLayout(null);
+		this.add(admin_panel);
+		
+		JMenuBar barra =new JMenuBar();
+		
+		JMenu menu_file=new JMenu("Archivo");
+		
+		JMenuItem open_item =new JMenuItem ("Abrir archivo...");
+		JMenuItem create_item =new JMenuItem ("Crear archivo...");
+		
+		barra.add(menu_file);
+		menu_file.add(open_item);
+		menu_file.add(create_item);
+		
+		this.setJMenuBar(barra);
+		//contenido del panel
+		JLabel regis=new JLabel("Usuarios",0);
+	    regis.setSize(160,60);
+	    regis.setFont(new Font("Arial",Font.BOLD,20));
+	    regis.setLocation(400,30);
+		regis.setOpaque(true);
+		regis.setBackground(Color.blue);
+		regis.setLayout(null);
+		admin_panel.add(regis);
+		this.add(admin_panel);
+		
+		JLabel title_widget=new JLabel("total de Usuarios");
+		title_widget.setBounds(60,80,200,100);
+		title_widget.setFont(new Font("Arial",Font.BOLD,20));
+		title_widget.setOpaque(false);
+		title_widget.setBackground(Color.white);
+		admin_panel.add(title_widget);
+		
+		JLabel usuarios_widget=new JLabel("100");
+		usuarios_widget.setBounds(120,120,200,100);
+		usuarios_widget.setFont(new Font("Arial",Font.BOLD,20));
+		usuarios_widget.setOpaque(false);
+		usuarios_widget.setBackground(Color.white);
+		admin_panel.add(usuarios_widget);
+		
+		JLabel widget=new JLabel("");
+		widget.setBounds(40,120,200,100);
+		widget.setOpaque(true);
+		widget.setBackground(Color.blue);
+		widget.setLayout(null);
+		admin_panel.add(widget);
+		
+		JButton exportar=new JButton ("Exportar");
+		exportar.setBounds(670,270,100,20);
+		admin_panel.add(exportar);
+		
+		JButton añadir=new JButton ("Añadir");
+		añadir.setBounds(770,270,100,20);
+		admin_panel.add(añadir);
+		
+		String title[]= {"No.control","nombre","apellidos","semestre","promedio","acciones"};
+		String tableData[][]= {
+				{"No.control","nombre","apellidos","semestre","promedio","acciones"},
+				{"No.control","nombre","apellidos","semestre","promedio","acciones"},
+				{"No.control","nombre","apellidos","semestre","promedio","acciones"},
+				{"No.control","nombre","apellidos","semestre","promedio","acciones"},
+				{"No.control","nombre","apellidos","semestre","promedio","acciones"},
+		};
+		JTable tabla=new JTable(tableData,title);
+		JScrollPane scroll=new JScrollPane (tabla);
+		scroll.setBounds(30,400,800,100);
+		admin_panel.add(scroll);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 
 }
